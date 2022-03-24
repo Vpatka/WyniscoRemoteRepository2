8536ee6b365feec3c315f3ac89d2ae773d08a184
Vaishali <patil.vaishuv26@gmail.com>
2022-03-23 19:35:46 -0600

    I changed my file which is modified at GITHUB also

--- RemoteFile1.java
+++ RemoteFile1.java
@@ -1,3 +1,4 @@
 Added the first line from GIT HUB
 Add Second line from local
 Switched to enhancement branch and added third line
+I have also done changes at my local when someone changed GIT HUB file which are not pushed
