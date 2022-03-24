283bc51646e1491bbaadebd7c2ab403853dfcbdc
Vaishali <patil.vaishuv26@gmail.com>
2022-03-23 19:56:39 -0600

    Revert "I changed my file which is modified at GITHUB also"
    This reverts commit 8536ee6b365feec3c315f3ac89d2ae773d08a184.
    
    # Conflicts:
    #	RemoteFile1.java

--- RemoteFile1.java
+++ RemoteFile1.java
@@ -1,8 +1,17 @@
 Added the first line from GIT HUB
 Add Second line from local
+<<<<<<< .mine
 Switched to enhancement branch and added third line
 <<<<<<< HEAD
 I have also done changes at my local when someone changed GIT HUB file which are not pushed
 =======
 Some other member have changed the GIT HUB file while I was changing at my local
 >>>>>>> 85a7bdce87aa0aa4e284bd0a97353e8a5958518c
+=======
+Switched to enhancement branch and added third line
+
+
+
+
+
+>>>>>>> .theirs
